74d3a254bd73755efd043a18cf7b31865f857a05
JojoVajco <137092699+Jojovajco@users.noreply.github.com>
2023-10-01 16:55:31 +0200

    Update cross_mumbai2fantom.js

--- scripts/cross_mumbai2fantom.js
+++ scripts/cross_mumbai2fantom.js
@@ -6,7 +6,7 @@ async function main() {
   const omniChainNFT = await OmniChainNFT.attach(
     "0x3cdB05ca336DbF64Fa091Bd660dBAd8106C686a6"
   );
-  await omniChainNFT.crossChain(
+  await omniChainNFT.crossChain()
     10012,
     "0xb03a572ee91aEcbdfa8ceF8196BF140A1E7410dF",
     ethers.BigNumber.from("1"),
